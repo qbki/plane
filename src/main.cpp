22e56c0fc4dd1dfbd3d2bf207d2c74f5c2bddf39
Sergey Andreev <andrejew.sw@gmail.com>
2023-07-15 23:22:45 +0300

    Removed unnecessary code

--- src/main.cpp
+++ src/main.cpp
@@ -65,13 +65,13 @@ int main() {
   // SDL_SetRelativeMouseMode(SDL_TRUE);
 
   std::unique_ptr<Camera> camera(new Camera(
-    glm::vec3(0.0, 0.0, 2.0),
+    {0.0, 0.0, 2.0},
     static_cast<float>(SCREEN_WIDTH) / SCREEN_HEIGHT
   ));
 
   std::unique_ptr<SunLight> light(new SunLight(
-    glm::vec3(1.0, 1.0, 1.0),
-    glm::vec3(0.2, 0.2, 1.0)
+    {1.0, 1.0, 1.0},
+    {0.2, 0.2, 1.0}
   ));
 
   auto model(Model::load(

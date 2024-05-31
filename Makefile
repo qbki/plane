4425a555e8abd5bc8bf69863e3c77dba65a730ed
Sergey Andreev <andrejew.sw@gmail.com>
2024-06-01 01:16:28 +0300

    Removed assets from wasm build

--- Makefile
+++ Makefile
@@ -84,6 +84,7 @@ run-debug: build-debug
 
 run-wasm: build-wasm build-web-shell
 	@cd $(WEB_SHELL_DIR) && \
+		ln -snf $(shell pwd)/assets assets && \
 		python3 -m http.server -b 127.0.0.1
 .PHONY: run-wasm
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,27 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { fileURLToPath } from 'url';
 import { resolve } from 'path';
 
+
 const ROOT_DIR = fileURLToPath(new URL('.', import.meta.url));
 function root(...args) {
   return resolve(ROOT_DIR, ...args);
 }
 
-/** @type {import('webpack').Configuration} */
-export default {
+/**
+ * @template T
+ * @param mode {import('webpack').Configuration["mode"]}
+ * @param dev_value {T}
+ * @param other_value {T}
+ * @return {T}
+ */
+function dev_select(mode, dev_value, other_value) {
+  return mode == "development" ? dev_value : other_value;
+}
+
+/**
+ * @param config {import('webpack').Configuration}
+ */
+export default (_, config) => ({
   entry: root('./src/web-shell/shell.ts'),
   module: {
     rules: [
@@ -32,6 +46,7 @@ export default {
   output: {
     filename: '[name].js',
     path: root('build/web-shell'),
+    clean: true,
   },
   optimization: {
     runtimeChunk: 'single',
@@ -43,11 +58,17 @@ export default {
     new CopyWebpackPlugin({
       patterns: [
         { from: root('shaders'), to: 'shaders' },
-        { from: root('assets/fonts'), to: 'assets/fonts' },
-        { from: root('assets/levels'), to: 'assets/levels' },
-        { from: root('assets/models'), to: 'assets/models' },
-        { from: root('assets/sound'), to: 'assets/sound' },
+        ...dev_select(
+          config.mode,
+          [
+            { from: root('assets/fonts'), to: 'assets/fonts' },
+            { from: root('assets/levels'), to: 'assets/levels' },
+            { from: root('assets/models'), to: 'assets/models' },
+            { from: root('assets/sound'), to: 'assets/sound' },
+          ],
+          [],
+        )
       ],
     }),
   ],
-};
+});

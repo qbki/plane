b597c6be80820954da09d5e437241d7d7e9b4835
Sergey Andreev <andrejew.sw@gmail.com>
2024-08-29 18:58:19 +0300

    Changed the level's directory root path

--- src/loader/levels_meta_loader.cpp
+++ src/loader/levels_meta_loader.cpp
@@ -13,7 +13,7 @@ load_levels_meta(const std::filesystem::path& levels_meta_path)
   auto json = load_json(levels_meta_path);
   auto files = json.at("levels").get<std::vector<std::string>>();
   auto files_view = files | std::views::transform([](const auto& path) {
-                      return LEVELS_DIR / path;
+                      return ASSETS_DIR / path;
                     });
   return { .levels{ files_view.begin(), files_view.end() } };
 }

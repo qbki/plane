dfffb48e64b1320241e9372ef88a0b3bfc778aaf
Sergey Andreev <andrejew.sw@gmail.com>
2024-12-18 05:19:20 +0300

    Adding an actor velocity to projectiles velocity

--- src/systems/gun.cpp
+++ src/systems/gun.cpp
@@ -28,13 +28,18 @@ gun_shooting_system(Scene& scene)
         registry, weapon.bullet_model_path);
       auto spread = weapon.spread * get_random_float() - weapon.spread * HALF;
 
-      ;
       auto direction = owner_transform.rotation()
                        * glm::quat(glm::vec3 { 0, 0, spread })
                        * glm::vec3(1, 0, 0);
       auto xy_direction = glm::normalize(
         glm::vec3(direction.x, direction.y, 0));
-      Velocity velocity { xy_direction * weapon.bullet_speed };
+
+      auto owner_velocity_wrapper = registry->try_get<Velocity>(owner_entity);
+      auto owner_velocity = owner_velocity_wrapper == nullptr
+                              ? glm::vec3(0, 0, 0)
+                              : owner_velocity_wrapper->value;
+
+      Velocity velocity { owner_velocity + xy_direction * weapon.bullet_speed };
 
       registry->replace<Velocity>(projectile_entity, velocity);
       registry->replace<Owner>(projectile_entity, owner_entity);
--- src/systems/level.cpp
+++ src/systems/level.cpp
@@ -9,9 +9,18 @@ level_boundaries_system(const Scene& scene)
 {
   auto& world_bbox = scene.state().world_bbox();
 
-  scene.state().registry().view<Transform, PlayerKind>().each(
-    [&](Transform& transform) {
+  scene.state().registry().view<Transform, Velocity, PlayerKind>().each(
+    [&](Transform& transform, Velocity& velocity) {
       auto pos = transform.translation();
+      if (pos.x > world_bbox.max.x || pos.x < world_bbox.min.x) {
+        velocity.value.x = 0;
+      }
+      if (pos.y > world_bbox.max.y || pos.y < world_bbox.min.y) {
+        velocity.value.y = 0;
+      }
+      if (pos.z > world_bbox.max.z || pos.z < world_bbox.min.z) {
+        velocity.value.z = 0;
+      }
       transform.translate({
         glm::clamp(pos.x, world_bbox.min.x, world_bbox.max.x),
         glm::clamp(pos.y, world_bbox.min.y, world_bbox.max.y),
